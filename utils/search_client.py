39a70a55febaef83d17cc223ae5385a0073d1baa
Gaspard Hassenforder <gbztamere69@gmail.com>
2025-07-26 13:44:35 +0200

    fix mplementation of search client

---
+++ utils/search_client.py
@@ -0,0 +1,22 @@
+from tavily import TavilyClient
+
+try:
+    from utils.config import TAVILY_API_KEY
+except ImportError:
+    from config import TAVILY_API_KEY
+
+
+tavily_client = TavilyClient(api_key=TAVILY_API_KEY)
+
+def search_tavily(query):
+    """Search using Tavily API
+    Args:
+        query: str - The search query
+    Returns:
+        dict - The search response from Tavily
+    """
+    response = tavily_client.search(query)
+    return response
+
+if __name__ == "__main__":
+    print(search_tavily("What is the capital of France?")) 
